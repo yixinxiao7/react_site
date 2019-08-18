7460f9de4a6fdc0205c124cfc7ab80199daafd11
Yixin Xiao <yixinx@umich.edu>
2019-08-18 15:26:37 -0400

    Updates

---
+++ precache-manifest.eba6153bd9937e5a3149cbc1b8d1bcf4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d3ba28c9a94effae0c39c0d53191b8",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "6c7c8f6589fe5b30653d",
+    "url": "/site/static/css/2.d819a5bb.chunk.css"
+  },
+  {
+    "revision": "58224354802cf32cff31",
+    "url": "/site/static/css/main.9b6f485e.chunk.css"
+  },
+  {
+    "revision": "6c7c8f6589fe5b30653d",
+    "url": "/site/static/js/2.a5b842c8.chunk.js"
+  },
+  {
+    "revision": "58224354802cf32cff31",
+    "url": "/site/static/js/main.33c843e8.chunk.js"
+  },
+  {
+    "revision": "14c83684653b1b55fe6d",
+    "url": "/site/static/js/runtime~main.de27f395.js"
+  },
+  {
+    "revision": "fae5033e63aaad02a5e7c6fa1fa9b11f",
+    "url": "/site/static/media/Resume.fae5033e.pdf"
+  },
+  {
+    "revision": "63fecf2de1e33142bf22b5a91dd97c6a",
+    "url": "/site/static/media/YX.63fecf2d.jpg"
+  },
+  {
+    "revision": "75eefc394b1a1145c2f3e5ea250c3d9b",
+    "url": "/site/static/media/black_image.75eefc39.jpg"
+  },
+  {
+    "revision": "36b88d5a333279940e33c51a2b616cfe",
+    "url": "/site/static/media/business.36b88d5a.jpg"
+  },
+  {
+    "revision": "713ef8141892cc70219c1cc66d0998d2",
+    "url": "/site/static/media/email.713ef814.jpg"
+  },
+  {
+    "revision": "5df4be28f35835ebc58f9055d7616da9",
+    "url": "/site/static/media/gb1.5df4be28.jpg"
+  },
+  {
+    "revision": "2ed4916f47ec2bf99a6f0cf02724e3e2",
+    "url": "/site/static/media/graph.2ed4916f.jpg"
+  },
+  {
+    "revision": "03a4fe69b527549f5819eeec7cea6785",
+    "url": "/site/static/media/log_mng.03a4fe69.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee3a3c16f95c92c7aeda24d1394c28c0",
+    "url": "/site/static/media/me_2.ee3a3c16.jpg"
+  },
+  {
+    "revision": "94faa96dd21789a40d6697ef8e34a7ee",
+    "url": "/site/static/media/phone.94faa96d.jpg"
+  },
+  {
+    "revision": "25477e2401891bf5a4d2c14b83b75743",
+    "url": "/site/static/media/puzzle.25477e24.jpg"
+  },
+  {
+    "revision": "f46f885382cf2a6fbdbdd5e9a43fa0df",
+    "url": "/site/static/media/stocks.f46f8853.jpg"
+  }
+]);
